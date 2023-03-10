9bfa5629c0449d4cb4834d71799b0497d32ce5ea
Sławek Zdunek <slawekzdunek2.3.6@gmail.com>
2023-03-10 16:36:26 +0100

    Bug Fix

--- LocalServer/bin/Apache24/htdocs/PytaniaMatura/submit.php
+++ LocalServer/bin/Apache24/htdocs/PytaniaMatura/submit.php
@@ -2,5 +2,5 @@
  
 $json = file_get_contents("php://input");
  
-if(strlen($json) > 1) file_put_contents("Answers.json",$json);
+if(strlen($json) > 1) file_put_contents("newAnswers.json",$json);
 ?>
